998cf44e4abf1772ce0fada18a483f43579a7064
SvenPaterson <stephenjgarden@icloud.com>
2025-10-01 17:17:41 -0400

    app built

---
+++ inspect_in_stream.py
@@ -0,0 +1,11 @@
+﻿import inspect
+import pkgutil
+import nidaqmx
+for importer, modname, ispkg in pkgutil.walk_packages(nidaqmx.__path__, prefix='nidaqmx.'):
+    if modname.endswith('in_stream'):
+        module = __import__(modname, fromlist=['InStream'])
+        InStream = getattr(module, 'InStream', None)
+        if InStream:
+            print(modname)
+            print(inspect.getsource(InStream.configure_logging))
+            break
